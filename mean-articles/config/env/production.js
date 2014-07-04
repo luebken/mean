55facf2d3cd72a79438bca370d4df4f9c3ad60c7
Matthias Luebken <matthias.luebken@gmail.com>
2014-07-04 13:22:19 +0200

    Env DB_HOST in production and test

--- mean-articles/config/env/production.js
+++ mean-articles/config/env/production.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var DB_HOST = process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost';
+
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/mean',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + DB_HOST + '/mean'
 };
